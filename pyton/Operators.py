2b74cb450c95260aa4541509131147cde02c1b1f
Pranavan-2009 <14062021studio@gmail.com>
2025-05-07 19:46:04 +0530

    py

---
+++ pyton/Operators.py
@@ -0,0 +1,30 @@
+name="Pranavan"
+age=16
+weight=75.2
+print("the value of name is",name)
+print("data type of name is",type(name))
+
+print("the value of name is",age)
+print("data type of name is",type(age))
+
+print("the value of name is",weight)
+print("data type of name is",type(weight))
+
+num1=12
+num2=10
+
+print("addition of num1 and num2 is",num1+num2)
+print("addition of num1 and num2 is",num1-num2)
+print("addition of num1 and num2 is",num1*num2)
+print("addition of num1 and num2 is",num1/num2)
+print("addition of num1 and num2 is",num1//num2)
+
+
+x=input("enter x value")
+y=input("enter y value")
+temp=x
+x=y
+y=temp
+
+print("x is ", x)
+print("y is ", y)
