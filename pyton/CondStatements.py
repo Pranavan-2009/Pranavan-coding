050a4b637b7d61824bb929ff3c4721ad9ad64c07
Pranavan-2009 <14062021studio@gmail.com>
2025-05-10 19:44:48 +0530

    py

---
+++ pyton/CondStatements.py
@@ -0,0 +1,8 @@
+no=int(input("enter the number to be cheked;"))
+if(no%2==0):
+   print("This is Even number")
+else:
+   print("This is Odd number")
+
+
+   
---
+++ pyton/DCnumber.py
@@ -0,0 +1,10 @@
+no=int(input("enter the number to be cheked;"))
+if(no>75):
+   
+   if(no%2==0):
+      print("This is an Even number")
+   else:
+      print("This is an Odd number")
+
+else:
+   print("This is less then 75")
