4938ffbb30eecfeb3b53870ddec9ff78fb80bd1a
Pranavan-2009 <14062021studio@gmail.com>
2025-05-10 19:56:09 +0530

    py

--- pyton/Statements 10.05.py
+++ pyton/Statements 10.05.py
@@ -3,6 +3,3 @@ if(no%2==0):
    print("This is Even number")
 else:
    print("This is Odd number")
-
-
-   
---
+++ pyton/dt.py
@@ -0,0 +1,7 @@
+import datetime
+dat=datetime.datetime.now()
+print(dat)                 
+
+import calendar
+cal=calendar.calendar(1985)
+print(cal)
