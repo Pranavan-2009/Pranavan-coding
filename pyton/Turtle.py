2d935d00f8aa81094561c3f457b30107ccc4eba0
Pranavan-2009 <14062021studio@gmail.com>
2025-05-21 19:55:07 +0530

    turtle

---
+++ pyton/Turtle.py
@@ -0,0 +1,28 @@
+import turtle
+
+sc= turtle.Screen()
+sc.bgcolor('yellow')
+sc.setup(500,500)
+board=turtle.Turtle()
+board.forward(100)
+board.left(120)
+board.forward(100)
+
+board.left(120)
+board.forward(100)
+
+board.penup()
+board.right(150)
+board.forward(50)
+
+board.pendown()
+board.right(90)
+board.forward(100)
+
+board.right(120)
+board.forward(100)
+
+board.right(120)
+board.forward(100)
+
+turtle.done()
