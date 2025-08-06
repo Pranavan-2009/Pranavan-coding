71b7e280571ccc99b8b99bf5644ae8a56bae607e
Pranavan-2009 <14062021studio@gmail.com>
2025-08-06 19:33:11 +0530

    06.08

--- d&s/02.08.2025.py
+++ d&s/02.08.2025.py
@@ -1,23 +1,23 @@
-# def printnumber(n):
-#     iteration=0
-#     print("The number enterd by user is ",n)
-#     iteration+=1
-#     print("Total iterations done by the code is ",iteration,"\n")
+def printnumber(n):
+    iteration=0
+    print("The number enterd by user is ",n)
+    iteration+=1
+    print("Total iterations done by the code is ",iteration,"\n")
 
-# printnumber(5)
-# printnumber(10)
-# print("\n with any 'n' the time taken by our code won't change")
+printnumber(5)
+printnumber(10)
+print("\n with any 'n' the time taken by our code won't change")
 
-# def onTime(n):
-#     iteration=0
-#     for i in range (1,n+1):
-#         iteration+=1
-#     print("when n is",n,"Iteratins =",iteration)
+def onTime(n):
+    iteration=0
+    for i in range (1,n+1):
+        iteration+=1
+    print("when n is",n,"Iteratins =",iteration)
 
-# onTime(10)
-# onTime(20)
-# onTime(50)
-# print("\n with every 'n' the time taken and iterations will increase")
+onTime(10)
+onTime(20)
+onTime(50)
+print("\n with every 'n' the time taken and iterations will increase")
 
 def ONSquareTime(n):
     iteration=0
---
+++ d&s/06.08.2025.py
@@ -0,0 +1,16 @@
+# def prints(n):
+#     if(n<=0):
+#         return
+#     print("codingal")
+#     prints(n)
+# prints(10)
+
+
+def arraysum(a):
+    sum=0
+    for i in a:
+        sum=sum+i
+    print(sum)
+
+a=[10,15,2,8]
+arraysum(a)
