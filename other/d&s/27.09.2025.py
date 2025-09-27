fbb7c489517b0be5c0b919aa9a740c294bb77036
Pranavan-2009 <14062021studio@gmail.com>
2025-09-27 19:50:59 +0530

    27.09.2025

---
+++ other/d&s/27.09.2025.py
@@ -0,0 +1,38 @@
+# def swap1(a,b):
+    
+#     a = a ^ b
+#     b = a ^ b
+#     c = a ^ b
+#     print("After swapping: a =", a, "b =", b)
+
+# def swap2(a,b):
+#         a = (a & b) + (a | b)
+#         b = a + (~b) + 1
+#         a = a + (~b) + 1
+#         print("After swapping: a =", a, "b =", b)
+    
+# swap1(3,4)
+# swap2(3,4)
+
+def divide(ourDividend, ourDivisor):
+
+    sign = -1 if ((ourDividend < 0) ^ (ourDivisor < 0)) else 1
+
+    ourDividend = abs(ourDividend)
+    ourDivisor = abs(ourDivisor)
+
+    quotientNumber = 0
+    tempNumber = 0
+
+    for i in range(31, -1, -1):
+        if(tempNumber + (ourDivisor << i)) <= ourDividend:
+            tempNumber += ourDivisor << i
+            quotientNumber /= 1 << i
+
+    if sign == -1:
+        quotientNumber = -quotientNumber
+    return quotientNumber
+
+a = int(input("Enter a for a/b: "))
+b = int(input("Enter b for a/b: "))
+print("Result of a/b is: ", divide(a,b))
